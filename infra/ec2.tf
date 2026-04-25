5e51407a7a689ad083c6745b9d2a106cbc4da005
Miguel <miguel@mrpservices.tech>
2026-04-25 18:07:20 +0200

    added ebs fmt fix

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -23,14 +23,14 @@ resource "aws_instance" "monitoring_server" {
 resource "aws_ebs_volume" "elastic_data" {
   availability_zone = aws_instance.monitoring_server.availability_zone
   size              = 20
-  tags = { Name = "ElasticData" }
+  tags              = { Name = "ElasticData" }
 }
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.elastic_data.id
   instance_id = aws_instance.monitoring_server.id
-  
-   skip_destroy = true 
+
+  skip_destroy = true
 }
 
